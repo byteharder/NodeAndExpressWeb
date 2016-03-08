b7751a50229b36d7070394b6ccf710d8243364f3
Vinmar Tipay <Vinmar@vinmars-mbp.home>
2016-03-08 21:28:25 +0000

    With a little bit of QA Testing

--- meadowlark.js
+++ meadowlark.js
@@ -14,13 +14,43 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' &&
+		req.query.test === '1';
+	next();
+});
+
+app.get('/', function(req, res) {
 	res.render('home');
 });
 
-app.get('/about', function(req, res){
-	res.render('about', {fortune: fortune.getFortune()});
+app.get('/headers', function(req, res){
+	res.set('Content-Type','text/plain');
+	var s = '';
+	for(var name in req.headers) s+= name + ': ' + req.headers[name] + '\n';
+	res.send(s);
 });
+
+app.get('/about', function(req,res){
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js' 
+	} );
+});
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
+});
+
+
+
+
 
 
 
